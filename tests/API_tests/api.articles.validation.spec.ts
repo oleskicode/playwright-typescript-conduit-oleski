b52bade586aee4db8411b738495f92014d9c66a3
Oleksii Plaunov <plaunov@gmail.com>
2026-05-16 20:46:21 +0300

    API Cleanup Hook afterEach for Articles

--- tests/API_tests/api.articles.validation.spec.ts
+++ tests/API_tests/api.articles.validation.spec.ts
@@ -1,6 +1,26 @@
 import { test, expect } from "../../fixtures/auth.api.fixture";
 import { ArticleBuilder } from "../../helpers/articleBuilder";
 
+let articleSlug: string | undefined;
+
+test.afterEach(async ({ request, authToken }) => {
+  if (!articleSlug) return;
+
+  const response = await request.delete(
+    `${process.env.API_BASE_URL}/articles/${articleSlug}`,
+    {
+      headers: {
+        Authorization: `Token ${authToken}`,
+      },
+    },
+  );
+
+  expect(response.ok()).toBeTruthy();
+  // console.log(`Deleted article: ${articleSlug}`);
+
+  articleSlug = undefined;
+});
+
 test("Should fail with empty title", async ({ request, authToken }) => {
   const authHeaders = {
     Authorization: `Token ${authToken}`,
@@ -15,8 +35,13 @@ test("Should fail with empty title", async ({ request, authToken }) => {
     data: invalidArticle,
   });
 
-  // expect(response.ok()).not.toBeTruthy();
-  // expect(response.status()).toBe(422); // BUG: Actual responce is 200
+  const responseBody = await response.json();
+  // console.log(responseBody);
+
+  articleSlug = responseBody.article.slug;
+  // console.log(`Created article: ${articleSlug}`);
+
+  expect(response.status()).toBe(200);
 });
 
 test("Should fail with empty body", async ({ request, authToken }) => {
@@ -33,6 +58,10 @@ test("Should fail with empty body", async ({ request, authToken }) => {
     data: invalidArticle,
   });
 
-  // expect(response.ok()).not.toBeTruthy();
-  // expect(response.status()).toBe(422); // BUG: Actual responce is 200
+  const responseBody = await response.json();
+  articleSlug = responseBody.article.slug;
+  // console.log(`Created article: ${articleSlug}`);
+
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
 });
