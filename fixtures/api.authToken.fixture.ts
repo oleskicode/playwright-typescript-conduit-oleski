10d88db723c019f8ad0628f467c64b87d78c469d
Oleksii Plaunov <plaunov@gmail.com>
2026-06-13 12:06:44 +0300

    authToken Fixture refactored into APIRequestContext authenticatedRequest fixture

--- fixtures/api.authToken.fixture.ts
+++ fixtures/api.authToken.fixture.ts
@@ -1,32 +1,69 @@
-import { test as base, expect } from "@playwright/test";
+import {
+  test as base,
+  expect,
+  APIRequestContext,
+  request as playwrightRequest,
+} from "@playwright/test";
 import dotenv from "dotenv";
 
+// Load env variables
 dotenv.config();
 
 type MyFixtures = {
   authToken: string;
+  authenticatedRequest: APIRequestContext;
 };
 
+// Helper function to fetch the authentication token.
+export async function getAuthTokenFn(
+  requestContext: APIRequestContext,
+): Promise<string> {
+  const baseURL = process.env.API_BASE_URL;
+
+  if (!baseURL) {
+    throw new Error("API_BASE_URL is not defined in the .env variables.");
+  }
+  const loginURL = `${baseURL}/users/login`;
+  const response = await requestContext.post(loginURL, {
+    data: {
+      user: {
+        email: process.env.USER_EMAIL,
+        password: process.env.USER_PASSWORD,
+      },
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+
+  const responseBody = await response.json();
+  const token = responseBody.user.token;
+  expect(token).toBeDefined();
+
+  return token;
+}
+
 export const test = base.extend<MyFixtures>({
+  // Use raw token if needed
   authToken: async ({ request }, use) => {
-    // --- SETUP (beforeEach logic) ---
-    const loginURL = `${process.env.API_BASE_URL}/users/login`;
-    const response = await request.post(loginURL, {
-      data: {
-        user: {
-          email: process.env.USER_EMAIL,
-          password: process.env.USER_PASSWORD,
-        },
+    // Get the token
+    const token = await getAuthTokenFn(request);
+    // Pass the token to the test
+    await use(token);
+  },
+
+  authenticatedRequest: async ({ authToken }, use) => {
+    // New context with authentication
+    const authenticatedContext = await playwrightRequest.newContext({
+      extraHTTPHeaders: {
+        Authorization: `Token ${authToken}`,
       },
     });
 
-    expect(response.ok()).toBeTruthy();
-    const responseBody = await response.json();
-    const token = responseBody.user.token;
-    expect(token).toBeDefined();
+    // Pass the authenticated context to the test
+    await use(authenticatedContext);
 
-    // Pass the token to the test
-    await use(token);
+    // Clean up
+    await authenticatedContext.dispose();
   },
 });
 
