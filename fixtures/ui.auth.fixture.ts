4f5d3591fa0b8aece9730e91f4c6cd92694a46b1
Oleksii Plaunov <plaunov@gmail.com>
2026-06-15 13:01:37 +0300

    uiAuthFixture is simplified with fetch token function now in helpers

--- fixtures/ui.auth.fixture.ts
+++ fixtures/ui.auth.fixture.ts
@@ -1,50 +1,25 @@
-import { test as base, expect } from "./ui.pages.fixture";
+import { test as base } from "./ui.pages.fixture";
+import { getAuthTokenFn } from "../helpers/apiGetAuthToken";
 
-// Extending base test to include setup and teardown
 export const test = base.extend({
   page: async ({ page, request }, use, testInfo) => {
-    // --- SETUP (beforeEach logic) ---
-    testInfo.annotations.push({
-      type: "Start time (logged from the fixture):",
-      description: new Date().toISOString(),
-    });
-
-    const requestURL = `${process.env.API_BASE_URL}/users/login`;
-    const response = await request.post(requestURL, {
-      data: {
-        user: {
-          email: process.env.USER_EMAIL,
-          password: process.env.USER_PASSWORD,
-        },
-      },
-    });
-    expect(response.ok).toBeTruthy();
-    const responseBody = await response.json();
-    const authToken = responseBody.user.token;
+    // Fetch Auth Token
+    const authToken = await getAuthTokenFn(request);
+    // Inject Auth Token into localStorage
     await page.addInitScript((token) => {
       window.localStorage.setItem("id_token", token);
     }, authToken);
 
-    // Provide the page to the test
+    // Pass authenticated page context into the test
     await use(page);
 
-    // --- TEARDOWN (afterEach logic) ---
-    testInfo.annotations.push({
-      type: "End Time (logged from the fixture):",
-      description: new Date().toISOString(),
-    });
-
+    //
     if (testInfo.status !== testInfo.expectedStatus) {
       const screenshot = await page.screenshot({ fullPage: true });
-      await testInfo.attach(
-        "Screenshot on failure (logged from the fixture):",
-        {
-          body: screenshot,
-          contentType: "image/png",
-        },
-      );
+      await testInfo.attach("screenshot-on-failure", {
+        body: screenshot,
+        contentType: "image/png",
+      });
     }
   },
 });
-
-export { expect } from "@playwright/test";
